b87676909c1f3a9fb27fea8a9e5c0932de20b90d
Doan Duc Manh <manhdoanduc7@gmail.com>
2024-09-27 00:16:37 +0700

    General-Knowledge

---
+++ B1-General-Knowledge/shared/main.c
@@ -0,0 +1,9 @@
+#include "./inc/hello_class.h"
+#include "./inc/hello_world.h"
+
+int main()
+{
+    hello_class();
+    hello_world();
+    return 0;
+}
---
+++ B1-General-Knowledge/shared/src/hello_class.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void hello_class()
+{
+    printf("Hello UET\n");
+}
